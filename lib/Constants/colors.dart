49ded8daafd31118670db217e404d1bda9d8c0b4
Abinash022 <abinashkarki16@gmail.com>
2024-07-04 10:44:54 +0545

    Initial project setup: folder structure, assets, and color palettes

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const bgColor = Color(0XFF212121);
+  static const appbarColor = Color(0XFF6A169B);
+  static const buttonColor = Color(0XFF481A63);
+  static const mainTextColor = Color(0XFFF0F0F0);
+  static const secondaryTextColor = Color(0XFF424242);
+}
